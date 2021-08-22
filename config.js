28a1b8a4091a476c240beb04b71f842ee00bcf40
vinhcute <89347390+vinhcute@users.noreply.github.com>
2021-08-22 21:31:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Huyền àaaaaa!',
+    introDesc: `HUyền như cục cứt trôi sông
+    Anh như con chó đứng trông bên bờ
+    Mây trôi,nước chảy hững hỡ
+    Con chó, cục cứt bao giờ gặp nhauu`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng HUyền đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Huyền đã say ngay từ lúc thấy nụ cười ấyyy ',
+    btnYes: 'Huyền iuu V ',
+    btnNo: 'Không, có cứt t thích m :3',
+    question:'Trên thế giới hơn 7 tỉ người mà răng Huyền đồng ý iu Vinhhh <3',
+    btnReply: 'Gửi cho Vinhh nầyy  <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Vinh biết mà trời, iu iuuu😘😘',
+    messDesc: 'Tối nay 10r ăn mỳ tômm nhaa.',
+    btnAccept: 'hmmmmm ok nhá <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
